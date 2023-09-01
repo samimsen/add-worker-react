3af3638faa33de9f2065c0888b6e82c654bb6f02
Samim Sen <samim.sen@hotmail.com>
2023-09-01 11:32:34 +0300

    useRef added

--- src/Workers/AddWorker.jsx
+++ src/Workers/AddWorker.jsx
@@ -1,48 +1,51 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Button from "../UI/Button"
 import Card from "../UI/Card"
 import ErrorModal from "../UI/ErrorModal"
 
 const AddWorker = ({ setWorkers }) => {
 
-    const [enteredName, setEnteredName] = useState("")
-    const [enteredSalary, setEnteredSalary] = useState("")
+    // const [enteredName, setEnteredName] = useState("")
+    // const [enteredSalary, setEnteredSalary] = useState("")
     const [error, setError] = useState()
-
+    const nameInputRef = useRef()
+    const salaryInputRef = useRef()
     const minimumSalary = 5000;
 
-    const workerNameChangeHandler = (e) => {
-        setEnteredName(e.target.value)
-    }
+    // const workerNameChangeHandler = (e) => {
+    //     setEnteredName(e.target.value)
+    // }
 
-    const workerSalaryChangeHandler = (e) => {
-        setEnteredSalary(e.target.value)
-    }
+    // const workerSalaryChangeHandler = (e) => {
+    //     setEnteredSalary(e.target.value)
+    // }
 
     const addWorkerHandler = (e) => {
 
         e.preventDefault()
 
-        if (enteredName.length === 0) {
+        if (nameInputRef.current.value.length === 0) {
             setError({ title: "İsim alanı zorunludur", message: "Lütfen bir isim giriniz" })
             return
         }
 
-        if (enteredSalary < minimumSalary) {
+        if (salaryInputRef.current.value < minimumSalary) {
             setError({ title: "Maaş alanı zorunludur", message: `Lütfen ${minimumSalary} değerinden büyük bir maaş değeri giriniz` })
             return
         }
 
         const addedWorker = {
             id: Math.floor(Math.random() * 1000),
-            name: enteredName,
-            salary: enteredSalary
+            name: nameInputRef.current.value,
+            salary: salaryInputRef.current.value
         }
 
         setWorkers(prevState => [addedWorker, ...prevState])
 
-        setEnteredName("")
-        setEnteredSalary("")
+        // setEnteredName("")
+        // setEnteredSalary("")
+        nameInputRef.current.value = ""
+        salaryInputRef.current.value =  ""
     }
 
     const errorHandler = () => {
@@ -64,8 +67,9 @@ const AddWorker = ({ setWorkers }) => {
                         id="name"
                         className="max-w-[40rem] w-full mx-auto border p-2"
                         placeholder="Çalışan ismi yazınız"
-                        onChange={workerNameChangeHandler}
-                        value={enteredName}
+                        // onChange={workerNameChangeHandler}
+                        // value={enteredName}
+                        ref={nameInputRef}
                     />
                     <label
                         htmlFor="salary"
@@ -77,8 +81,9 @@ const AddWorker = ({ setWorkers }) => {
                         id="salary"
                         className="max-w-[40rem] w-full mx-auto border p-2"
                         placeholder="Maaş miktarı yazınız"
-                        onChange={workerSalaryChangeHandler}
-                        value={enteredSalary}
+                        // onChange={workerSalaryChangeHandler}
+                        // value={enteredSalary}
+                        ref={salaryInputRef}
                     />
                     <Button
                         type="submit"
